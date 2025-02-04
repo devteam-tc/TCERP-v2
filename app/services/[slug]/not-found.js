48716d82bd274bc606b9a4abc3793189134ab18b
devteam-tc <devteam.tc@gmail.com>
2025-02-04 16:25:29 +0530

    Removed unused CSS and JavaScript files for industries and services pages

--- app/services/[slug]/not-found.js
+++ app/services/[slug]/not-found.js
@@ -1,10 +1,10 @@
-// /industries/[slug]/not-found.js
+// /services/[slug]/not-found.js
 
 export default function NotFound() {
     return (
       <div>
-        <h1>Industry not found</h1>
-        <p>Sorry, we could not find the details for the industry you are looking for.</p>
+        <h1>services not found</h1>
+        <p>Sorry, we could not find the details for the services you are looking for.</p>
       </div>
     );
   }
